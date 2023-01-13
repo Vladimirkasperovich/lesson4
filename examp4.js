952cd761115aa9e2c37e35733e4c49e839d1de1b
vladimir kasperovich <92395305+Vladimirkasperovich@users.noreply.github.com>
2023-01-13 13:20:41 +0300

    lesson4
    lesson4

---
+++ examp4.js
@@ -0,0 +1,28 @@
+// function nextInLine(arr, item) {
+//     arr.push(item);
+//     let arr1 = arr.shift();
+    
+//     return arr1
+//     // Only change code above this line
+//   }
+  
+//   // Setup
+//   let testArr = [1, 2, 3, 4, 5];
+  
+//   // Display code
+//   console.log("Before: " + JSON.stringify(testArr));
+//   console.log(nextInLine(testArr, 6));
+//   console.log("After: " + JSON.stringify(testArr));
+
+/********************************************************************************************** */
+// function welcomeToBooleans() {
+//     // Only change code below this line
+//     if (true) {
+//       return true;
+  
+//    }
+//       return false; // Change this line
+    
+//     // Only change code above this line
+//   }
+/************************************************************************************************************* */
